4c5645dd7939fe0529e9d832fcc1f5eb98ba1194
Kevin <kcabrer1@asu.edu>
2016-10-01 20:11:30 -0700

    Updated peripheral code for XBee communication

--- Peripheral_Code/arduino_code/ShuttersMain.ino
+++ Peripheral_Code/arduino_code/ShuttersMain.ino
@@ -1,29 +1,60 @@
+#include <SoftwareSerial.h>
 #include "Temp.h"
 #include "Light.h"
 
 int tempSensorPin = 0; // Required by the function that gets the temp reading
+//SoftwareSerial XBee(0, 1); // RX, TX
 
 void setup() {
   Serial.begin(9600); 
+  //XBee.begin(9600); // Not too sure what this is doing
   light_setup();  
 }
 
 void loop() {
-  /************************************
-  ***** Check for manual commands *****
-  ************************************/ 
+  /***********************************
+  ***** Check for manual inputs *****
+  ***********************************/ 
+  /*while (Serial.available()) {
+    Serial.print("Stuff came in\n");
+    Serial.print(Serial.read());
+    Serial.print("\n");
+  }*/
 
+  while (Serial.available()) {
+    Serial.println(Serial.read()); // Reads any incoming data from RPi
+  }
 
   /**********************************
   ***** Get light/temp readings *****
   **********************************/
-  float tempReading = get_temp_reading(tempSensorPin);
-  float lightReading = get_light_reading();
+  int tempReading = (int)get_temp_reading(tempSensorPin);
+  int lightReading = (int)get_light_reading();
 
-  // Print temp/light data to monitor
-  Serial.print(tempReading); Serial.println(" degrees Fahrenheit");
-  Serial.print(lightReading); Serial.println(" lux");
+  // Creates data string in the appropriate format for tranmissions to RPi
+  String outputData = format_data("1", tempReading, lightReading);
+
+  // Prints data to serial monitor which causes a broadcast to all XBee devices
+  Serial.print(outputData);
+
+  // Found code that used the XBee.command() format but I'm
+  // not sure what it does, since Serial.command() already 
+  // takes care of broadcasting the data
+  //XBee.write(tempReading);
+  //XBee.write(lightReading);
+  //XBee.println("5 seconds have passed");
 
   // Sleep 5 seconds
   delay(5000);
+}
+
+String format_data(String moduleID, int tempReading, int lightReading) {
+  String data = moduleID;
+  char reading[3];
+  sprintf(reading, "%03d", tempReading);
+  data += reading;
+  sprintf(reading, "%03d", lightReading);
+  data += reading; 
+  data += "\n";
+  return data;
 }
