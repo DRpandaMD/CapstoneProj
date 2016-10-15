651c2a1815f95ee20e77a19d90e0fd71ceab3331
moxr <moxr@users.noreply.github.com>
2016-10-15 12:06:40 -0700

    Add Android WebView app

---
+++ Android-App/ClimateControlShutters/app/src/main/java/io/github/moxr/climatecontrolshutters/MainActivity.java
@@ -0,0 +1,64 @@
+package io.github.moxr.climatecontrolshutters;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+
+        // open all links within WebView
+        myWebView.setWebViewClient(new WebViewClient());
+
+        // Javascript in case we need it
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // URL goes here
+        myWebView.loadUrl("http://example.com");
+
+
+    }
+
+    // make back button navigate history
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    // In case we want a menu or settings
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
