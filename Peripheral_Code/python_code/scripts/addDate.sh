73ad638611bb1bf8711decc0fad2cd7a9a78bc2f
Kevin <kcabrer1@asu.edu>
2016-10-28 18:12:45 -0700

    Added/Modified RPi scripts
    Fixed errors in the addDate.sh and delDataFile.sh scripts. The command to create the data files was written incorrectly.
    Created turnModuleOFF.py and turnModuleON.py scripts to broadcast signals to the Arduinos.

--- Peripheral_Code/python_code/scripts/addDate.sh
+++ Peripheral_Code/python_code/scripts/addDate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/temp/[0-8]_temp >/dev/null
-echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/light/[0-8]_light >/dev/null
+echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/temp/{1..6}_temp >/dev/null
+echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/light/{1..6}_light >/dev/null
--- Peripheral_Code/python_code/scripts/delDataFile.sh
+++ Peripheral_Code/python_code/scripts/delDataFile.sh
@@ -5,8 +5,10 @@ rm /home/pi/Documents/Shutters_Scripts/temp/*_temp >/dev/null
 rm /home/pi/Documents/Shutters_Scripts/light/*_light >/dev/null
 
 # Create data files
-touch /home/pi/Documents/Shutters_Scripts/temp/{1..8}_temp
-touch /home/pi/Documents/Shutters_Scripts/light/{1..8}_light
+touch /home/pi/Documents/Shutters_Scripts/temp/{1..6}_temp
+touch /home/pi/Documents/Shutters_Scripts/light/{1..6}_light
+
+# Adds search directions to light and temp files
 echo "To search for a date type '/dd-mm-yyyy<enter>'" | tee \
-	/home/pi/Documents/Shutters_Scripts/temp/{1..8}_temp \
-	/home/pi/Documents/Shutters_Scripts/light/{1..8}_light >/dev/null
+	/home/pi/Documents/Shutters_Scripts/temp/{1..6}_temp \
+	/home/pi/Documents/Shutters_Scripts/light/{1..6}_light >/dev/null
