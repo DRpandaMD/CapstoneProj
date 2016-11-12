e1979b99c6930cf1987708026826c972f180b243
DRpandaMD <michael.zarate.ce@gmail.com>
2016-11-12 12:32:38 -0700

    Please go back and check your local repos aginst the master

---
+++ Peripheral_Code/arduino_code/servo.ino
@@ -0,0 +1,44 @@
+/******************************************************************************
+servo-skatch.ino
+Example sketch for connecting a hobby servo to a sparkfun redboard
+  (https://www.sparkfun.com/products/9065)
+  (https://www.sparkfun.com/products/12757)
+Byron Jacquot@ SparkFun Electronics
+May 17, 2016
+
+**SparkFun code, firmware, and software is released under the MIT License(http://opensource.org/licenses/MIT).**
+
+Development environment specifics:
+Arduino 1.6.5
+******************************************************************************/
+
+#include <Servo.h>
+
+Servo myservo;
+
+uint32_t next;
+bool state;
+void setup()
+{
+  // the 1000 & 2000 set the pulse width 
+  // mix & max limits, in microseconds.
+  // Be careful with shorter or longer pulses.
+  myservo.attach(9, 1000, 2000);
+  //digital 9 pin on arduino
+}
+
+void loop()
+{
+  if(state)
+    {
+      myservo.write(180);
+      state = false;
+    }
+    else
+    {
+      myservo.write(0);
+      state = true;
+      
+    }
+  }
+
--- Peripheral_Code/python_code/scripts/addDate.sh
+++ Peripheral_Code/python_code/scripts/addDate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/temp/{1..6}_temp >/dev/null
-echo $(date +"%d-%m-%Y") | tee /home/pi/Documents/Shutters_Scripts/light/{1..6}_light >/dev/null
+
+echo $(date +"%d-%m-%Y") | tee -a /home/pi/Documents/Shutter_Project/sensor_data/module_{1..6} >/dev/null
