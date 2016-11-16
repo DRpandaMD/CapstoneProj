f79adde952f43296f5de4910b1775427ec6da210
DRpandaMD <michael.zarate.ce@gmail.com>
2016-11-15 19:45:42 -0700

    Finished with user sign-up

--- Climate-Control-Shutters-Web-App/Climate-Control-Shutters/test/integration/users_signup_test.rb
+++ Climate-Control-Shutters-Web-App/Climate-Control-Shutters/test/integration/users_signup_test.rb
@@ -4,11 +4,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #start testing for User Sign up
    test "invalid signup information " do
    	get signup_path
-   	assert_no_difference 'User.count' do 
-   		post users_path, user: {name: "",
-   								email: "user@invalid",
-   								password: 				"foo",
-   								password_confirmation:  "bar"}
+   	assert_no_difference 'User.count', 1 do 
+   		post_via redirect users_path, user: {name: "Example User",
+   								                         email: "user@example.com",
+   								                         password: 				      "foo",
+   								                         password_confirmation: "bar" }
    	end
    	assert_template 'users/new'
   end
